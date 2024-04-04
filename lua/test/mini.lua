3253a46fc1c87d9cef2fbbadc314d9c6cddcf5af
GhoulBoii <github@ghoulcloud.slmail.me>
2024-04-04 20:52:21 +0530

    chore: remove test

--- lua/test/mini.lua
+++
@@ -1,7 +0,0 @@
-require('mini.comment').setup()
-require('mini.cursorword').setup()
-require('mini.pairs').setup()
-require('mini.indentscope').setup({
-  symbol = '│',
-  options = { try_as_border = true },
-})
