a50c1e1a6ec3ae32a7ca3ccdbc4cfd8c2b03fac6
GhoulBoii <github@ghoulcloud.slmail.me>
2024-04-06 00:54:08 +0530

    feat(whichkey): adding which-key.nvim

---
+++ lua/plugins/whichkey.lua
@@ -0,0 +1,27 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    presets = {
+      operators = false,
+      motions = false,
+      text_objects = false,
+      windows = false,
+      nav = false,
+      z = false,
+      g = false,
+    },
+    window = {
+      border = "rounded",
+      position = "bottom",
+      padding = { 2, 2, 2, 2 },
+    },
+    ignore_missing = true,
+    show_help = false,
+    show_keys = false,
+    disable = {
+      buftypes = {},
+      filetypes = { "TelescopePrompt" },
+    },
+  },
+}
