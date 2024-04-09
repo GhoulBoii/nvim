41ae68b9e0f920c7f3564782e26cb99f647aca31
GhoulBoii <github@ghoulcloud.slmail.me>
2024-04-10 00:14:14 +0530

    feat(plugins): adding lazy keybind

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -12,5 +12,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.runtimepath:prepend(lazypath)
 
+vim.api.nvim_set_keymap("n", "<leader>l", "<cmd>Lazy<cr>", { noremap = true, desc = "Lazy", silent = true })
+
 -- Plugins
 require("lazy").setup("plugins")
