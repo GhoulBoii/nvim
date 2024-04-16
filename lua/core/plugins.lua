a1cc53f29ee411ce45af79090aec42439ed2a518
GhoulBoii <github@ghoulcloud.slmail.me>
2024-04-16 20:15:43 +0530

    feat(plugins): lazy config

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -15,4 +15,19 @@ vim.opt.runtimepath:prepend(lazypath)
 vim.api.nvim_set_keymap("n", "<leader>l", "<cmd>Lazy<cr>", { noremap = true, desc = "Lazy", silent = true })
 
 -- Plugins
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+  checker = { enabled = true },
+  concurrency = 5,
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "netrwPlugin",
+        "tarPlugin",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+  debug = false,
+})
