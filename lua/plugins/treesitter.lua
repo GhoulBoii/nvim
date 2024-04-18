2cedd90e08d44cf3c81a7b946feb41ee20ef7c63
GhoulBoii <github@ghoulcloud.slmail.me>
2024-04-19 00:19:13 +0530

    style(treesitter): formatting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,20 +3,14 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     init = function(plugin)
-      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-      -- no longer trigger the **nvim-treesitter** module to be loaded in time.
-      -- Luckily, the only things that those plugins need are the custom queries, which we make available
-      -- during startup.
       require("lazy.core.loader").add_to_rtp(plugin)
       require("nvim-treesitter.query_predicates")
     end,
     dependencies = {
       {
+        "JoosepAlviste/nvim-ts-context-commentstring",
         "nvim-treesitter/nvim-treesitter-textobjects",
         config = function()
-          -- When in diff mode, we want to use the default
-          -- vim text objects c & C instead of the treesitter ones.
           local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
           local configs = require("nvim-treesitter.configs")
           for name, fn in pairs(move) do
@@ -104,13 +98,5 @@ return {
       end
       require("nvim-treesitter.configs").setup(opts)
     end,
-  },
-
-  {
-    "JoosepAlviste/nvim-ts-context-commentstring",
-    lazy = true,
-    opts = {
-      enable_autocmd = false,
-    },
   },
 }
