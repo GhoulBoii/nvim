055c6e7f064e1228204651e46eeac5f2bff5382c
GhoulBoii <github@ghoulcloud.slmail.me>
2024-08-20 22:44:55 +0530

    feat(markdown): peek -> markview

--- lua/plugins/peek.lua
+++ lua/plugins/peek.lua
@@ -1,10 +1,20 @@
+-- return {
+--   "toppair/peek.nvim",
+--   ft = "markdown",
+--   build = "deno task --quiet build:fast",
+--   config = function()
+--     require("peek").setup()
+--     vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+--     vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+--   end,
+-- }
+
 return {
-  "toppair/peek.nvim",
+  "OXY2DEV/markview.nvim",
   ft = "markdown",
-  build = "deno task --quiet build:fast",
-  config = function()
-    require("peek").setup()
-    vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-    vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-  end,
+
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons"
+  }
 }
