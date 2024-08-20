2be596d3095136b1872bc9c2e88fc0e9e5b74ea0
GhoulBoii <github@ghoulcloud.slmail.me>
2024-08-20 22:44:04 +0530

    fix(statusline): getWords() works on txt and md

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,46 +1,51 @@
+local function getWords()
+  local filetype = vim.bo.filetype
+  if filetype == "text" or filetype == "" or filetype == "markdown" then
+    local wordcount = vim.fn.wordcount()
+    local visual_words = wordcount.visual_words
+    local words = visual_words or wordcount.words
+
+    if visual_words == 1 then
+      return "1 word"
+    else
+      return tostring(words) .. " words"
+    end
+  end
+  return ""
+end
+
 return {
   {
     "nvim-lualine/lualine.nvim",
-    config = function()
-      local function getWords()
-        if vim.bo.buftype ~= "nofile" or vim.bo.filetype == "markdown" then
-          if vim.fn.wordcount().visual_words == 1 then
-            return tostring(vim.fn.wordcount().visual_words) .. " word"
-          elseif not (vim.fn.wordcount().visual_words == nil) then
-            return tostring(vim.fn.wordcount().visual_words) .. " words"
-          else
-            return tostring(vim.fn.wordcount().words) .. " words"
-          end
-        else
-          return ""
-        end
-      end
-
-      require('lualine').setup {
-        options = {
-          theme = pywal,
-          component_separators = '|',
-          section_separators = { left = '', right = '' },
-        },
-        sections = {
-          lualine_a = { 'mode' },
-          lualine_b = { 'filename', 'branch' },
-          lualine_c = {},
-          lualine_x = {},
-          lualine_y = { 'filetype', getWords },
-          lualine_z = { 'progress' },
-        },
-        inactive_sections = {
-          lualine_a = { 'filename' },
-          lualine_b = {},
-          lualine_c = {},
-          lualine_x = {},
-          lualine_y = {},
-          lualine_z = { 'location' },
-        },
-        tabline = {},
-        extensions = {},
-      }
-    end
+    opts = {
+      options = {
+        theme = pywal,
+        component_separators = '|',
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = {
+          statusline = {
+            "ministarter",
+          }
+        }
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'filename', 'branch' },
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = { 'filetype', getWords },
+        lualine_z = { 'progress' },
+      },
+      inactive_sections = {
+        lualine_a = { 'filename' },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { 'location' },
+      },
+      tabline = {},
+      extensions = {},
+    }
   },
 }
