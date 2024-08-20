e724fc64cc2edbff86703bdf4fd9786526287454
GhoulBoii <github@ghoulcloud.slmail.me>
2024-08-20 22:30:01 +0530

    feat(filetype/python): remove redundant python conf

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,7 +1,4 @@
 -- use pep8 standards
-vim.opt_local.expandtab = true
-vim.opt_local.shiftwidth = 4
-vim.opt_local.tabstop = 4
 vim.opt_local.softtabstop = 4
 
 -- folds based on indentation https://neovim.io/doc/user/fold.html#fold-indent
