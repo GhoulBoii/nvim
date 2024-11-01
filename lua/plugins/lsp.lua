30da484f5e5187b2bcb0658afa34bcd760348401
GhoulBoii <github@ghoulcloud.slmail.me>
2024-11-01 13:46:24 +0530

    feat(lsp): update lsp-zero to v4

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,49 +1,9 @@
 return {
   {
     'VonHeikemen/lsp-zero.nvim',
-    branch = "v3.x",
+    branch = 'v4.x',
     lazy = true,
     config = false,
-    init = function()
-      vim.g.lsp_zero_extend_cmp = 0
-      vim.g.lsp_zero_extend_lspconfig = 0
-    end,
-  },
-
-  {
-    'williamboman/mason.nvim',
-    lazy = false,
-    config = true,
-  },
-
-  {
-    "garymjr/nvim-snippets",
-    dependencies = {
-      "rafamadriz/friendly-snippets",
-    },
-    opts = {
-      friendly_snippets = true,
-    },
-    keys = {
-      {
-        "<Tab>",
-        function()
-          return vim.snippet.active({ direction = 1 }) and "<cmd>lua vim.snippet.jump(1)<cr>" or "<Tab>"
-        end,
-        expr = true,
-        silent = true,
-        mode = { "i", "s" },
-      },
-      {
-        "<S-Tab>",
-        function()
-          return vim.snippet.active({ direction = -1 }) and "<cmd>lua vim.snippet.jump(-1)<cr>" or "<S-Tab>"
-        end,
-        expr = true,
-        silent = true,
-        mode = { "i", "s" },
-      },
-    },
   },
 
   -- Autocompletion
@@ -51,50 +11,173 @@ return {
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     dependencies = {
-      "garymjr/nvim-snippets",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'saadparwaiz1/cmp_luasnip',
+      { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
+      'rafamadriz/friendly-snippets',
     },
     config = function()
-      local lsp_zero = require('lsp-zero')
-      lsp_zero.extend_cmp()
-
       local cmp = require('cmp')
-      local cmp_action = lsp_zero.cmp_action()
+      local cmp_action = require('lsp-zero').cmp_action()
+      local luasnip = require('luasnip')
+
+      -- Load friendly-snippets
+      require('luasnip.loaders.from_vscode').lazy_load()
 
       cmp.setup({
         sources = {
-          { name = 'nvim_lsp' },
-          { name = "snippets" },
-          { name = 'buffer' },
-          { name = 'path' },
+          { name = 'nvim_lsp', priority = 1000 },
+          { name = 'luasnip',  priority = 750 },
+          { name = 'buffer',   priority = 500 },
+          { name = 'path',     priority = 250 },
         },
 
-        snippet = {
-          expand = function(args)
-            vim.snippet.expand(args.body)
-          end,
+        -- Window appearance
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
         },
 
-        formatting = lsp_zero.cmp_format({}),
+        -- Improved mapping for a better completion experience
         mapping = cmp.mapping.preset.insert({
-          ['<Tab>'] = cmp_action.tab_complete(),
-          ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
-          ['<CR>'] = cmp.mapping.confirm({ select = false }),
+          ['<Tab>'] = cmp_action.luasnip_supertab(),
+          ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-u>'] = cmp.mapping.scroll_docs(-4),
           ['<C-d>'] = cmp.mapping.scroll_docs(4),
-          -- ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-          -- ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-        })
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }),
+          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
+        }),
+
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+
+        -- Add borders to completion menu
+        formatting = {
+          format = function(entry, vim_item)
+            vim_item.menu = ({
+              nvim_lsp = "[LSP]",
+              luasnip = "[Snippet]",
+              buffer = "[Buffer]",
+              path = "[Path]",
+            })[entry.source.name]
+            return vim_item
+          end
+        },
       })
     end
+  },
+
+  -- LSP
+  {
+    'neovim/nvim-lspconfig',
+    cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+      { 'folke/lazydev.nvim',               ft = "lua", opts = {} },
+    },
+    config = function()
+      -- LSP Diagnostics configuration
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        update_in_insert = false,
+        underline = true,
+        severity_sort = true,
+        float = {
+          border = 'rounded',
+          source = 'always',
+          header = '',
+          prefix = '',
+        },
+      })
+
+      -- Add diagnostic symbols
+      local signs = { Error = " ", Warn = " ", Hint = "󰌵 ", Info = " " }
+      for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+      end
+
+      local lsp_defaults = require('lspconfig').util.default_config
+
+      lsp_defaults.capabilities = vim.tbl_deep_extend(
+        'force',
+        lsp_defaults.capabilities,
+        require('cmp_nvim_lsp').default_capabilities()
+      )
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        desc = 'LSP actions',
+        callback = function(event)
+          local opts = { buffer = event.buf, noremap = true, silent = true }
+
+          -- Enhanced keymaps
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, opts)
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+          vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, opts)
+          vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
+          vim.keymap.set({ 'n', 'x' }, '<F3>', function() vim.lsp.buf.format({ async = true }) end, opts)
+          vim.keymap.set('n', '<F4>', vim.lsp.buf.code_action, opts)
+
+          -- Diagnostic keymaps
+          vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
+          vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+          vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+        end,
+      })
+
+      -- Configure specific language servers
+      local lspconfig = require('lspconfig')
+
+      -- Lua LSP configuration
+      lspconfig.lua_ls.setup({
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { 'vim' }
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false,
+            },
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
+      })
+    end,
+  },
+
+  -- Mason
+  {
+    'williamboman/mason.nvim',
+    lazy = false,
+    config = true,
   },
 
   {
     'WhoIsSethDaniel/mason-tool-installer.nvim',
     dependencies = {
-      { "williamboman/mason.nvim" },
+      { "williamboman/mason.nvim",          config = true },
       { "williamboman/mason-lspconfig.nvim" },
     },
     cmd = "Mason",
@@ -103,70 +186,14 @@ return {
         "clangd",
         "jdtls",
         "lua_ls",
-        "pyright",  -- LSP for python
-        "ruff-lsp", -- linter for python (includes flake8, pep8, etc.)
-        "debugpy",  -- debugger
-        "black",    -- formatter
-        "isort",    -- organize imports
-        "taplo",    -- LSP for toml (for pyproject.toml files)
+        "basedpyright",
+        "ruff-lsp",
+        "debugpy",
+        "black",
+        "isort",
+        "taplo",
       },
+      auto_update = true,
     },
-  },
-
-
-  {
-    'neovim/nvim-lspconfig',
-    cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
-    event = { 'BufReadPre', 'BufNewFile' },
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'williamboman/mason-lspconfig.nvim',
-      {
-        "folke/lazydev.nvim",
-        ft = "lua",
-        opts = {},
-      },
-    },
-    config = function()
-      local lsp_zero = require('lsp-zero')
-      lsp_zero.extend_lspconfig()
-
-      lsp_zero.on_attach(function(_, bufnr)
-        lsp_zero.default_keymaps({ buffer = bufnr, preserve_mappings = false })
-        lsp_zero.buffer_autoformat()
-      end)
-      lsp_zero.set_sign_icons({
-        error = '✘',
-        warn = '▲',
-        hint = '⚑',
-        info = '»'
-      })
-      local opts = {
-        diagnostics = {
-          update_in_insert = false,
-          virtual_text = {
-            spacing = 4,
-            source = "if_many",
-            prefix = "●",
-          },
-        },
-        inlay_hints = {
-          enabled = true,
-        },
-      }
-
-      vim.diagnostic.config(opts)
-
-      require('mason-lspconfig').setup({
-        handlers = {
-          lsp_zero.default_setup,
-          lua_ls = function()
-            local lua_opts = lsp_zero.nvim_lua_ls()
-            require('lspconfig').lua_ls.setup(lua_opts)
-          end,
-        }
-      })
-    end
   }
 }
-
